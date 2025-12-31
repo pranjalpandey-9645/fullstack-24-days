1affe098c18fcea8901b8ca02764f6421eb59339
Pranjal Pandey <ppranjal573@gmail.com>
2025-12-31 23:47:31 +0530

    feat: comment out background image in CSS and clean up console logs in JavaScript

--- day-5/Js/script.js
+++ day-5/Js/script.js
@@ -1,3 +1,22 @@
 alert('Hello, DAEMON!');
-console.log("pranjal......code is running")
+console.log("......code is running")
 console.log("hey it's me daemon the great and the leader for the real.")
+
+var a = 1
+var b = 3
+var c = 7
+
+console.log(a + b + c - 8)
+
+var a = prompt("Enter your web address")
+var isTrue = confirm("do you want to leave this page?")
+
+if (isTrue){
+    console.log("Your pc is hacked!!!")
+}
+
+else{
+    console.log("Huh you are safe now, I think I need to hack your pc to get you stuck there!")
+}
+
+console.log("your address is" + a)
