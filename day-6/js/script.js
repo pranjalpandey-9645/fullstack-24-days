2943c84761bfb21c9f656aa2e207a34d64d51e25
Pranjal Pandey <ppranjal573@gmail.com>
2026-01-01 19:10:39 +0530

    feat: format CSS styles for consistency and improve JavaScript alert functionality

--- day-6/js/script.js
+++ day-6/js/script.js
@@ -1,1 +1,13 @@
 alert("Hello, World!");
+
+var a = prompt("This is a msg tab just to show you!")
+var isTrue = confirm("Are you Enjoying your journey on this page!?")
+
+if (isTrue) {
+    console.log("Thanks for your feedback!")
+}
+else{
+    console.log("We felt sorry for it!")
+} 
+
+console.log("Code is running!........")
