90454532f6b4c80eee12969ba35ea879970a0c14
pranjalpandey-9645 <ppranjal573@gmail.com>
2026-01-16 21:52:37 +0530

    feat: Add the day-13 folder.

---
+++ day-13/script.js
@@ -0,0 +1,16 @@
+console.log("Day 13: JavaScript if else statements");
+
+let message;
+message = "Hello, World!";
+
+console.log(message);
+
+let method;
+method = 3+29-50/60;
+
+console.log(method);
+
+let names;
+names = [Anime, cruelity, brutality, nobelity];
+
+console.log(names);
