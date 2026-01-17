2c0b55d358c49a0d8d9dcde428b474e9bdeed97a
pranjalpandey-9645 <ppranjal573@gmail.com>
2026-01-18 01:17:51 +0530

    feat: Add day-14 folder with script.js

---
+++ day-14/script.js
@@ -0,0 +1,20 @@
+console.log("Day 14: Script Loaded");
+
+let age = 17;
+
+if (age == 20) {
+    console.log("you are able to earn.");
+} 
+else if (age >= 19) {
+    console.log("you will be able to earn if you wait a bit and focus on you skills first🧑‍💻.");
+} 
+else {
+    console.log("you are able to earn if and only if you wait a bit and focus on your studies📚.");
+}
+
+let a = 20;
+b = 30;
+c = 92;
+d = a+b>c ? "a+b is greater than c" : "a+b is less than c";
+
+console.log(d);
