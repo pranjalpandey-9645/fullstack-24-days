e0d53b5bd762f65b47a6cf28363a70ae9be8f09b
pranjalpandey-9645 <ppranjal573@gmail.com>
2026-01-21 18:20:11 +0530

    refactor: code structure.

--- day-15/script.js
+++ day-15/script.js
@@ -9,6 +9,24 @@ if (number === product) {
     console.log("The number is not equal to the product.");
 }
 
+
+// Now we are going to learn the difference between '==' and '==='
+let a = 5;
+let b = '5';
+
+if (a == b) {
+    console.log("a is equal to b using '=='.");
+}
+/*if (a === b) {
+    console.log("a is equal to b using '==='.");
+    
+    This is commented out to avoid confusion, but if uncommented, 
+    it would show that '===' checks for both value and type equality.
+    }*/
+else {
+    console.log("a is not equal to b using '=='.");
+}
+
 // Case sensensitivity of javascript
 let greeting = "Hello, World!";
 console.log(greeting); // Correct case
@@ -18,20 +36,3 @@ console.log(Greeting); // Different variable due to case sensitivity
 
 let GREETING = "Good day!";
 console.log(GREETING); // Another different variable due to case sensitivity
-
-// Now we are going to learn the difference between '==' and '==='
-let a = 5;
-let b = '5';
-
-if (a == b) {
-    console.log("a is equal to b using '=='.");
-} 
-/*if (a === b) {
-    console.log("a is equal to b using '==='.");
-
-    This is commented out to avoid confusion, but if uncommented, 
-    it would show that '===' checks for both value and type equality.
-}*/
-else {
-    console.log("a is not equal to b using '=='.");
-}
