ecb91f666e8fad4926908833461588180febaa97
pranjalpandey-9645 <ppranjal573@gmail.com>
2026-01-24 00:00:03 +0530

    feat: add index.js for day-18 module with welcome message and current date.

---
+++ day-18/index.js
@@ -0,0 +1,4 @@
+console.log("Day 18 module loaded");
+
+console.log(`Current Date and Time: ${new Date().toLocaleString()}`);
+console.log("Hey there! Welcome to Day 18 of Fullstack 50 Days Challenge.");
