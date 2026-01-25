cef0ffe7cfff5708a2d6875485ed69dad2300201
pranjalpandey-9645 <ppranjal573@gmail.com>
2026-01-25 21:54:54 +0530

    feat: Add function call to execute changePrimitive and produce console output

--- day-17/script.js
+++ day-17/script.js
@@ -5,13 +5,14 @@ function nice(Ultron) {
     console.log("Hello, " + Ultron + " you're nice!");
     console.log("Hello, " + Ultron + " have a great day!");
 }
+nice("Ultron");
 
 function sum(a, b, c = 3) {
     console.log("Calculating the sum...");
     return a + b + c;
 }
 
-result = sum(1, 2);
+result = sum(1, 2, 8);
 console.log("The result is: " + result);
 
 function myfunc(theObject) {
@@ -26,4 +27,17 @@ const mycar = {
 
 console.log("Before myfunc call: " + mycar.make);
 myfunc(mycar);
-console.log("After myfunc call: " + mycar.make);
+console.log("After make call: " + mycar.model);
+console.log("After model call: " + mycar.year);
+
+const primValue = 5;
+
+function changePrimitive(primValue) {
+    console.log("Before change: " + primValue);
+    primValue = 7;
+    console.log("After change: " + primValue);
+    console.log("Inside function, primValue is: " + primValue);
+}
+
+changePrimitive(primValue);
+console.log("After changePrimitive call, primValue is: " + primValue);
